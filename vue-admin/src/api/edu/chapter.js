b1d5170f616c31a8d25bf72c67c9fffe7ddb4b8c
ChaR <1215585066@qq.com>
2022-02-15 10:26:16 +0800

    feat: 课程大纲列表查询（章节、小节）

---
+++ vue-admin/src/api/edu/chapter.js
@@ -0,0 +1,12 @@
+import request from '@/utils/request' //引入已经封装好的axios 和 拦截器
+
+export default{
+    //根据课程id获取章节和小节数据列表
+    getChapterVideoByCourseId(courseId){
+        return request({
+            url:`/eduService/chapter/getChapterVideo/${courseId}`,
+            method: 'get',
+        })
+    },
+
+}
--- vue-admin/src/views/edu/course/chapter.vue
+++ vue-admin/src/views/edu/course/chapter.vue
@@ -12,11 +12,46 @@ bottom: 40px;"
       <el-step title="创建课程大纲" />
       <el-step title="最终发布" />
     </el-steps>
+
+     <!-- 章节 -->
+<ul class="chanpterList">
+   <li
+       v-for="chapter in chapterVideoList"
+       :key="chapter.id">
+       <p>
+           {{ chapter.title }}
+
+           <span class="acts">
+               <el-button style="" type="text" @click="openVideo(chapter.id)">添加小节</el-button>
+               <el-button style="" type="text" @click="openEditChatper(chapter.id)">编辑</el-button>
+               <el-button type="text" @click="removeChapter(chapter.id)">删除</el-button>
+           </span>
+       </p>
+
+       <!-- 视频 -->
+       <ul class="chanpterList videoList">
+           <li
+               v-for="video in chapter.children"
+               :key="video.id">
+               <p>{{ video.title }}
+
+           <span class="acts">
+               
+               <el-button style="" type="text" @click="openEditVideo(video.id)">编辑</el-button>
+               <el-button type="text" @click="removeVideo(video.id)">删除</el-button>
+           </span>
+               </p>
+           </li>
+       </ul>
+   </li>
+</ul>
+
+
     <el-form label-width="120px">
       <el-form-item>
         <el-button @click="previous">上一步</el-button>
         <el-button :disabled="saveBtnDisabled" type="primary" @click="next"
-          >下 一步</el-button
+          >下一步</el-button
         >
       </el-form-item>
     </el-form>
@@ -24,9 +59,14 @@ bottom: 40px;"
 </template>
 
 <script>
+import chapter from '@/api/edu/chapter.js';
+
+
 export default {
   data() {
     return {
+      courseId:'',
+      chapterVideoList:[],
       saveBtnDisabled: false,
     };
   },
@@ -39,10 +79,64 @@ export default {
       //跳转到第三步
       this.$router.push({ path: "/course/publish/1" });
     },
+     //根据课程id查询对应的课程章节和小结
+    getChapterVideoByCourseId(){
+        chapter.getChapterVideoByCourseId(this.courseId)
+          .then(resp=>{
+            this.chapterVideoList =  resp.data.allChapterVideo
+          })
+    },
+
+    
   },
-  created() {},
+  created() {
+      //获取路由里的id值
+      if(this.$route.params && this.$route.params.id){
+       this.courseId = this.$route.params.id
+      }
+      //根据课程id查询对应的课程章节和小结
+      this.getChapterVideoByCourseId();
+    }
 };
 </script>
 
-<style>
+<style scoped>
+.chanpterList{
+    position: relative;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+.chanpterList li{
+  position: relative;
+}
+.chanpterList p{
+  float: left;
+  font-size: 20px;
+  margin: 10px 0;
+  padding: 10px;
+  height: 70px;
+  line-height: 50px;
+  width: 100%;
+  border: 1px solid #DDD;
+}
+.chanpterList .acts {
+    float: right;
+    font-size: 14px;
+}
+
+.videoList{
+  padding-left: 50px;
+}
+.videoList p{
+  float: left;
+  font-size: 14px;
+  margin: 10px 0;
+  padding: 10px;
+  height: 50px;
+  line-height: 30px;
+  width: 100%;
+  border: 1px dotted #DDD;
+}
 </style>
+
