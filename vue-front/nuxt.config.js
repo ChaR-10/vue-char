57a36f373250c6a479e77e6593c25c31430b603b
ChaR <1215585066@qq.com>
2022-02-23 22:58:34 +0800

    feat：首页-vue-awesome-swiper-实现轮播图效果

--- vue-front/nuxt.config.js
+++ vue-front/nuxt.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  
+  // some nuxt config...
+  plugins: [
+    { src: '~/plugins/nuxt-swiper-plugin.js', ssr: false }
+  ],
+
+  css: [
+    'swiper/dist/css/swiper.css'
+  ],
   /*
   ** Headers of the page
   */
